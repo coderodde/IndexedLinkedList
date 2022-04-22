379572861ba0fcbf593cadf8cb97be957e43fa7d
Rodion Efremov <coderodd3@yandex.com>
2022-04-22 09:08:02 +0300

    Working on SubList.

--- src/main/java/com/github/coderodde/util/EnhancedLinkedList.java
+++ src/main/java/com/github/coderodde/util/EnhancedLinkedList.java
@@ -1,5 +1,6 @@
 package com.github.coderodde.util;
 
+import java.util.AbstractList;
 import java.util.AbstractSequentialList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -124,6 +125,17 @@ public class EnhancedLinkedList<E>
             }
 
             return finger.node;
+        }
+        
+        ////////////////////////////////////////////////////////////////////////
+        // Removes the last ´numberOfFingers´ fingers from this finger list. 
+        ////////////////////////////////////////////////////////////////////////
+        void removeTrailingFingers(int numberOfFingers) {
+            Arrays.fill(fingerArray, size - numberOfFingers, size, null);
+            fingerArray[size - numberOfFingers] = fingerArray[size];
+            fingerArray[size] = null;
+            fingerArray[size].index -= numberOfFingers;
+            contractFingerArrayIfNeeded(size);
         }
         
         // Appends the input finger to the tail of the finger list:
@@ -963,31 +975,6 @@ public class EnhancedLinkedList<E>
         }
     }
     
-    // Used for checkInvariant()
-    private static <E> int countLeft(Finger<E> finger) {
-        Node<E> node = finger.node.prev;
-        int count = 0;
-        
-        while (node != null) {
-            node = node.prev;
-            count++;
-        }
-        
-        return count;
-    }
-    
-    // Used for checkInvariant()
-    private static <E> int countRight(Finger<E> finger) {
-        Node<E> node = finger.node.next;
-        int count = 0;
-        
-        while (node != null) {
-            node = node.next;
-            count++;
-        }
-        
-        return count;
-    }
     
     private void decreaseSize() {
         size--;
@@ -1592,13 +1579,14 @@ public class EnhancedLinkedList<E>
         }
     }
     
-    /***************************************************************************
-    Subtracts 'steps' positions from each index at least 'startingIndex'.
-    ***************************************************************************/
-    private void shiftIndicesToLeftOnce(int startFingerIndex) {
-        for (int sz = fingerList.size(), i = startFingerIndex; i <= sz; ++i) {
-            fingerList.get(i).index--;
-        }
+    ////////////////////////////////////////////////////////////////////////////
+    // Removes a range from this list.
+    ////////////////////////////////////////////////////////////////////////////
+    protected void removeRange(int fromIndex, int toIndex) {
+        int removalSize = toIndex - fromIndex;
+        int nextFingerCount = getRecommendedNumberOfFingers(size - removalSize);
+        
+        fingerList.removeTrailingFingers(fingerList.size() - nextFingerCount);
     }
     
     /***************************************************************************
@@ -1989,6 +1977,193 @@ public class EnhancedLinkedList<E>
                     
                 default -> false;
             };
+        }
+    }
+    
+    private static class SubList<E> extends AbstractList<E> {
+        
+        private final EnhancedLinkedList<E> root;
+        private final SubList<E> parent;
+        private final int offset;
+        protected int size;
+        
+        public SubList(EnhancedLinkedList<E> root, int fromIndex, int toIndex) {
+            this.root = root;
+            this.parent = null;
+            this.offset = fromIndex;
+            this.size = toIndex - fromIndex;
+            this.modCount = root.modCount;
+        }
+        
+        public SubList(SubList<E> parent, int fromIndex, int toIndex) {
+            this.root = parent.root;
+            this.parent = parent;
+            this.offset = parent.offset + fromIndex;
+            this.size = toIndex - fromIndex;
+            this.modCount = root.modCount;
+        }
+        
+        public void add(int index, E element) {
+            checkInsertionIndex(index);
+            checkForComodification();
+            root.add(offset + index, element);
+            updateSizeAndModCount(1);
+        }
+        
+        public boolean addAll(Collection<? extends E> c) {
+            return addAll(size, c);
+        }
+        
+        public boolean addAll(int index, Collection<? extends E> collection) {
+            checkInsertionIndex(index);
+            int collectionSize = collection.size();
+            
+            if (collectionSize == 0) {
+                return false;
+            }
+            
+            checkForComodification();
+            root.addAll(offset + index, collection);
+            updateSizeAndModCount(collectionSize);
+            return true;
+        }
+        
+        public E get(int index) {
+            Objects.checkIndex(index, size);
+            checkForComodification();
+            return root.get(offset + index);
+        }
+        
+        public Iterator<E> iterator() {
+            return listIterator();
+        }
+        
+        public ListIterator<E> listIterator(int index) {
+            checkForComodification();
+            checkInsertionIndex(index);
+            
+            return new ListIterator<E>() {
+                private final ListIterator<E> i = 
+                        root.listIterator(offset + index);
+                
+                public boolean hasNext() {
+                    return nextIndex() < size;
+                }
+                
+                public E next() {
+                    if (hasNext()) {
+                        return i.next();
+                    } 
+                    
+                    throw new NoSuchElementException();
+                }
+                
+                public boolean hasPrevious() {
+                    return previousIndex() >= 0;
+                }
+                
+                public E previous() {
+                    if (hasPrevious()) {
+                        return i.previous();
+                    }
+                    
+                    throw new NoSuchElementException();
+                }
+                
+                public int nextIndex() {
+                    return i.nextIndex() - offset;
+                }
+                
+                public int previousIndex() {
+                    return i.previousIndex() - offset;
+                }
+                
+                public void remove() {
+                    i.remove();
+                    updateSizeAndModCount(-1);
+                }
+                
+                public void set(E e) {
+                    i.set(e);
+                }
+                
+                public void add(E e) {
+                    i.add(e);
+                    updateSizeAndModCount(1);
+                }
+            };
+        }
+        
+        public E remove(int index) {
+            Objects.checkIndex(index, size);
+            checkForComodification();
+            E result = root.remove(offset + index);
+            updateSizeAndModCount(-1);
+            return result;
+        }
+        
+        public E set(int index, E element) {
+            Objects.checkIndex(index, size);
+            checkForComodification();
+            return root.set(offset + index, element);
+        }
+        
+        public int size() {
+            checkForComodification();
+            return size;
+        }
+        
+        public List<E> subList(int fromIndex, int toIndex) {
+            subListRangeCheck(fromIndex, toIndex, size);
+            return new SubList<>(this, fromIndex, toIndex);
+        }
+        
+        protected void removeRange(int fromIndex, int toIndex) {
+            checkForComodification();
+            root.removeRange(offset + fromIndex, offset + toIndex);
+            updateSizeAndModCount(fromIndex - toIndex);
+        }
+        
+        private void checkForComodification() {
+            if (root.modCount != this.modCount)
+                throw new ConcurrentModificationException();
+        }
+
+        private void checkInsertionIndex(int index) {
+            if (index < 0) {
+                throw new IndexOutOfBoundsException(index);
+            }
+            
+            if (index > size) {
+                throw new IndexOutOfBoundsException(
+                        "index(" + index + ") > size(" + size + ")");
+            }
+        }
+        
+        static void subListRangeCheck(int fromIndex, int toIndex, int size) {
+            if (fromIndex < 0) {
+                throw new IndexOutOfBoundsException("fromIndex = " + fromIndex);
+            }
+            
+            if (toIndex > size){
+                throw new IndexOutOfBoundsException(
+                        "toIndex(" + toIndex + ") > size(" + size + ")");
+            }
+                
+            if (fromIndex > toIndex)
+                throw new IllegalArgumentException(
+                        "fromIndex(" + fromIndex + ") > toIndex(" 
+                                + toIndex + ")");
+        }
+        
+        private void updateSizeAndModCount(int sizeDelta) {
+            SubList<E> subList = this;
+            
+            do {
+                subList.size += sizeDelta;
+                subList.modCount = root.modCount;
+                subList = subList.parent;
+            } while (subList != null);
         }
     }
 }
